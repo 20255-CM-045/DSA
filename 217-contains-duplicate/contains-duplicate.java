9f96d14936182a58532f4420c18aba495216ff9b
Lakshmi <119479549+20255-CM-045@users.noreply.github.com>
2025-08-19 11:55:14 +0530

    Time: 20 ms (28.99%) | Memory: 58 MB (84.58%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,6 +1,29 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> hs=new HashSet<>();
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                return true;
+            }
+        }
+        return false;
+        /*ArrayList<Integer> al=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i:nums)
+        {
+            if(al.contains(i))
+            {
+                return true;
+            }
+            else
+            {
+                al.add(i);
+            }
+        }
+        return false;*/
+       /* HashSet<Integer> hs=new HashSet<>();
         for(int i:nums)
         {
             if(hs.contains(i))
@@ -12,6 +35,6 @@ class Solution {
                 hs.add(i);
             }
         }
-        return false;
+        return false;*/
     }
 }
